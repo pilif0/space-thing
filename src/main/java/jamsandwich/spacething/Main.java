2531876191ce635ba095f191250237106251623d
Euan Hunter <euanhunter117@gmail.com>
2017-02-25 12:37:11 +0000

    Added LWJGL dependencies.

--- src/main/java/jamsandwich/spacething/Main.java
+++ src/main/java/jamsandwich/spacething/Main.java
@@ -1,7 +1,9 @@
 package jamsandwich.spacething;
 
+import org.lwjgl.glfw.GLFW;
+
 public class Main {
     public static void main(final String[] args) {
-        System.out.println("Space thing!");
+        System.out.println(GLFW.glfwGetVersionString());
     }
 }
